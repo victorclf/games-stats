d07571cd018128dca4e773e3cdea7b0471fc0cf7
Victor Freire <victorclf@live.com>
2022-10-03 23:43:35 -0300

    Add jsdoc comments to services

--- backend/src/services/gamesService.ts
+++ backend/src/services/gamesService.ts
@@ -11,9 +11,18 @@ export interface SearchResult {
 export default class GamesService {
 	@Inject()
 	private rawgService: RAWGService;
-	
-	async search({ search, page, page_size}: { search?: string, page?: number, page_size?: number}): Promise<SearchResult[]> {
-		const { results } = await this.rawgService.getGames({ search, page, page_size});
-		return results.map(({name, background_image: backgroundImage, released: releaseDate }) => ({name, backgroundImage, releaseDate}));
+
+	/**
+	 * Search games.
+	 * 
+	 * @param {Object} query - query object
+	 * @param {string} query.search - Search query.
+	 * @param {number} query.page - A page number within the paginated result set.
+	 * @param {number} query.page_size - Number of results to return per page.
+	 * @returns {Promise<SearchResult[]} Promise with an array of search results
+	 */
+	async search({ search, page, page_size }: { search?: string, page?: number, page_size?: number }): Promise<SearchResult[]> {
+		const { results } = await this.rawgService.getGames({ search, page, page_size });
+		return results.map(({ name, background_image: backgroundImage, released: releaseDate }) => ({ name, backgroundImage, releaseDate }));
 	}
 }
--- backend/src/services/rawgService.ts
+++ backend/src/services/rawgService.ts
@@ -1,3 +1,12 @@
 export default abstract class RAWGService {
-	abstract getGames({ search, page, page_size}: { search?: string, page?: number, page_size?: number}): Promise<any>;
+	/**
+	 * RAWG API endpoint. Get a list of games.
+	 * 
+	 * @param {Object} query - query object
+	 * @param {string} query.search - Search query.
+	 * @param {number} query.page - A page number within the paginated result set.
+	 * @param {number} query.page_size - Number of results to return per page.
+	 * @return {Promise<any>} list of games
+	 */
+	abstract getGames({ search, page, page_size }: { search?: string, page?: number, page_size?: number }): Promise<any>;
 }
